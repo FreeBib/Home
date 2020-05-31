3e9d662e0e3fbd89f82237582d709e5520a3687c
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 06:33:54 -0400

    Code sample page should work properly now

--- Views/CodeSample.php
+++ Views/CodeSample.php
@@ -1,5 +1,5 @@
 <?php require_once('Views/Components/Header.php'); ?>
 <main>
-    <?php foreach($db->query('SELECT code FROM CodeExamples') as $code_sample) { echo $code_sample; } ?>
+    <?php foreach($db->query('SELECT code FROM CodeExamples') as $code_sample) { echo $code_sample[0]; } ?>
 </main>
 <?php require_once('Views/Components/Footer.php'); ?>
