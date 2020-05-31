b2f6b5ecf7479a0b0e93d2a244b55e26b490d4cf
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 08:58:11 +0000

    Added css and js files

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
     <title><?= SITE_NAME ?></title>
+
+    <!-- CSS Stylesheets -->
+    <link rel="stylesheet" href="res/css/main.css">
+
+    <!-- Preloaded Javascript -->
+    <script src="res/js/main.js"></script>
 </head>
 <body>
     <main>
