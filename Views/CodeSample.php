c2944a4690d584d96712b116d7a5c67adacb6293
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 07:48:33 -0400

    Working on code samples infrastructure

--- Views/CodeSample.php
+++ Views/CodeSample.php
@@ -5,7 +5,11 @@ require_once('Views/Components/Header.php');
 if (!isset($_GET['id']) || empty($_GET['id'])) {
     echo "<p style='color: red; text-style: bold;'>Example not found.</p>";
 } else {
-
+    $statement = $db->prepare('SELECT * FROM CodeSamples WHERE id=:id');
+    $statement->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+    $statement->execute();
+    $code_sample = $statement->fetch();
+    $statement->closeCursor();
 ?>
 <main>
     <div class="codeblock" style="width: 950px; max-width: 95%;">
@@ -17,22 +21,33 @@ if (!isset($_GET['id']) || empty($_GET['id'])) {
                 <tbody>
                     <?php
 
-                    $line_number = 0;
-        
-                    foreach ($db->query('SELECT code FROM CodeExamples') as $code_sample) {
-                        $line = strtok($code_sample[0], "\n");
+                    $line = strtok($code_sample[0], "\n");
 
-                        while ($line) {
-                            printf("<tr class=\"line\">");
-                            printf("<td class=\"line-numbers\">%d</td>", ++$line_number);
-                            printf("<td class=\"line-content\">%s</td>", $line);
-                            printf("</tr>");
+                    while ($line) {
+                        printf("<tr class=\"line\">");
+                        printf("<td class=\"line-numbers\">%d</td>", ++$line_number);
+                        printf("<td class=\"line-content\">%s</td>", $line);
+                        printf("</tr>");
 
-                            $line = strtok("\n");
-                        }
-
-                        break;
+                        $line = strtok("\n");
                     }
+
+                    // $line_number = 0;
+        
+                    // foreach ($db->query('SELECT code FROM CodeExamples') as $code_sample) {
+                    //     $line = strtok($code_sample[0], "\n");
+
+                    //     while ($line) {
+                    //         printf("<tr class=\"line\">");
+                    //         printf("<td class=\"line-numbers\">%d</td>", ++$line_number);
+                    //         printf("<td class=\"line-content\">%s</td>", $line);
+                    //         printf("</tr>");
+
+                    //         $line = strtok("\n");
+                    //     }
+
+                    //     break;
+                    // }
         
                     ?>
                 </tbody>
