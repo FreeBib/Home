5e28c8b50d63a4f9430d25dd30001a35a8beffeb
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 07:34:04 -0400

    Navigation footer corrected

--- Views/Components/Footer.php
+++ Views/Components/Footer.php
@@ -1,6 +1,6 @@
     <footer style="text-align: center;">
-    <a href="/">Home</a> | 
-        <a href="/CodeSample.php">Code Examples</a>
+        <a href="/">Home</a> | 
+        <a href="/CodeSamples.php">Code Examples</a>
         <br />
         <p id="request-uri"><?= $uri ?></p>
     </footer>
