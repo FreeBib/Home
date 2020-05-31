5737884d8dd268f916328102547a494c14f2b6ce
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 06:07:53 -0400

    Removed code sample from the main page

--- Views/Home.php
+++ Views/Home.php
@@ -22,14 +22,6 @@ $uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRING);
     <main>
         <p>Welcome to FreeBib.org!</p>
         <p>The goal of this site is to help make intermediate- to advanced-level information in Mathematics and Computer Science accessible to anyone, regardless of whether they can afford it. I myself could not have made it to where I am today without the support of countless individuals, many of whom do not even know about the profundity of the impact they had on me. As far as they know, they gave a random person some advice one day, and probably never thought about it again. This website was born out of the desire to continue that chain of altruistic pedagogy, and I make all of the information on this site completely free for your edification, whoever you are.</p>
-        <?php
-        echo "<pre class=\"code\">";
-foreach ($db->query('SELECT * FROM CodeExamples') as $example) {
-    echo $example[2];
-}
-echo "</pre>";
-
-        ?>
     </main>
 
     <footer style="text-align: center;">
@@ -39,4 +31,3 @@ echo "</pre>";
     </footer>
 </body>
 </html>
-
