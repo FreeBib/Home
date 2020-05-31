6bfbb3fb6f67cc08a95f4df577e36cda517de0c4
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 08:25:19 -0400

    Created Courses directory

---
+++ Courses/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title>FreeBib.org</title>
+
+    <!-- CSS Stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Questrial&family=Source+Code+Pro&display=swap">
+    <link rel="stylesheet" href="res/css/main.css">
+
+    <!-- Preloaded Javascript -->
+    <script src="res/js/main.js"></script>
+</head>
+<body>
+    <ul>
+        <li>x86-64</li>
+        <li>C</li>
+        <li>C++</li>
+        <li>Python</li>
+    </ul>
+</body>
+</html>
--- Views/Components/Footer.php
+++ Views/Components/Footer.php
@@ -1,6 +1,7 @@
     <footer style="text-align: center;">
         <a href="/">Home</a> | 
-        <a href="/CodeSamples.php">Code Examples</a>
+        <a href="/CodeSamples.php">Code Examples</a> | 
+        <a href="/Courses/">Courses</a>
         <br />
         <p id="request-uri"><?= $uri ?></p>
     </footer>
