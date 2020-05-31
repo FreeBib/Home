dede23e19c313b14bb5cba71e9e2f4916259d8a5
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 06:46:21 -0400

    Working on code formatting

--- Views/CodeSample.php
+++ Views/CodeSample.php
@@ -1,5 +1,16 @@
 <?php require_once('Views/Components/Header.php'); ?>
 <main>
-    <?php foreach ($db->query('SELECT code FROM CodeExamples') as $code_sample) { echo "<pre class=\"code\">$code_sample[0];</pre>"; } ?>
+    <div class="code-block">
+        <div class="code">
+        <?php
+        
+        foreach ($db->query('SELECT code FROM CodeExamples') as $code_sample) {
+            // echo "<pre class=\"code\">$code_sample[0]</pre>";
+            echo $code_sample[0];
+        }
+        
+        ?>
+        </div>
+    </div>
 </main>
 <?php require_once('Views/Components/Footer.php'); ?>
