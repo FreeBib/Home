ce7284cf2b9ed9389faa61ae28a3247fa3c5143c
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-31 08:44:20 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php define( 'SITE_NAME', 'FreeBib.org' ); ?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title><?= SITE_NAME ?></title>
+</head>
+<body>
+    <main>
+        <p>Welcome to FreeBib.org!</p>
+    </main>
+</body>
+</html>
+
